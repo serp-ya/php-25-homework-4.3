1a8d2b2b33392b5caeb8e2b0a3b65d3fa924852f
serp-ya <sergei.kundryukov1989@gmail.com>
2018-08-06 21:54:06 +0300

    Добавил проверку авторизации на главной

--- index.php
+++ index.php
@@ -1,26 +1,30 @@
 <?php
 date_default_timezone_set('UTC');
-require_once('./Controller.class.php');
+$userId;
 
-$controller = new Controller();
-$taskData = $controller->getData();
-$isEdit = false;
-$editedId;
-$editedDescription;
+if (!empty($_SESSION['userid'])) {
+  require_once('./Controller.class.php');
+  $userId = $_SESSION['userid'];
 
-if (!empty($_GET['action']) && $_GET['action'] === 'change') {
-  $isEdit = true;
-  $editedId = $_GET['id'];
-}
+  $controller = new Controller();
+  $taskData = $controller->getData();
+  $isEdit = false;
+  $editedId;
+  $editedDescription;
 
-if ($isEdit) {
-  foreach ($taskData as $task) {
-    if ((int) $task['id'] === (int) $editedId) {
-      $editedDescription = $task['description'];
+  if (!empty($_GET['action']) && $_GET['action'] === 'change') {
+    $isEdit = true;
+    $editedId = $_GET['id'];
+  }
+
+  if ($isEdit) {
+    foreach ($taskData as $task) {
+      if ((int) $task['id'] === (int) $editedId) {
+        $editedDescription = $task['description'];
+      }
     }
   }
 }
-
 ?>
 
 <!DOCTYPE html>
@@ -31,72 +35,84 @@ if ($isEdit) {
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
   <title>hw-4-2</title>
 </head>
+
 <body style="margin: 0 auto; width: 960px;">
+<?php if (empty($userId)): ?>
+  <a href="/registry.php" style="padding: 10px; background-color: lightgray;">
+    Войти
+  </a>
 
-<h1>Список дел:</h1>
-  
-<div style="display: flex; justify-content: space-around;">
-  <form method="POST">
-    <?php if ($isEdit): ?>
-      <input 
-        type="text" 
-        name="description" 
-        placeholder="Описание задачи"
-        value="<?php echo $editedDescription; ?>"
-      >
-      <input type="hidden" name="id" value="<?php echo $editedId;?>">
-      <input type="submit" value="Сохранить">
-    <?php else: ?>
-      <input type="text" name="new-task" placeholder="Описание задачи">
-      <input type="submit" value="Добавить">
-    <?php endif; ?>
-  </form>
+<?php else: ?>
+  <h1>Список дел:</h1>
+  <div style="display: flex; justify-content: space-around;">
+    <form method="POST">
+      <?php if ($isEdit): ?>
+
+        <input 
+          type="text" 
+          name="description" 
+          placeholder="Описание задачи"
+          value="<?php echo $editedDescription; ?>"
+        >
+        <input type="hidden" name="id" value="<?php echo $editedId;?>">
+        <input type="submit" value="Сохранить">
+
+      <?php else: ?>
+
+        <input type="text" name="new-task" placeholder="Описание задачи">
+        <input type="submit" value="Добавить">
+
+      <?php endif; ?>
+    </form>
 
 
-  <form method="POST">
-    Сортировать по: 
-    <select name="sort-by">
-      <option value="date_added">
-        Дате добавления
-      </option>
-    
-      <option value="is_done">
-        Статусу
-      </option>
-    
-      <option value="description">
-        Описанию
-      </option>
-    </select>
-    <input type="submit" value="Отсортировать">
-  </form>
-</div>
+    <form method="POST">
+      Сортировать по: 
+      <select name="sort-by">
 
-<table style="width: 100%">
-  <thead style="background-color: #bfbfbf;">
-    <tr>
-      <td>Описание задачи</td>
-      <td>Дата добпаления</td>
-      <td>Статус</td>
-      <td></td>
-    </tr>
-  </thead>
+        <option value="date_added">
+          Дате добавления
+        </option>
+        <option value="is_done">
+          Статусу
+        </option>
+        <option value="description">
+          Описанию
+        </option>
 
-  <tbody>
-    <?php foreach($taskData as $task): ?>
+      </select>
+      <input type="submit" value="Отсортировать">
+    </form>
+  </div>
+
+  <table style="width: 100%">
+    <thead style="background-color: #bfbfbf;">
       <tr>
-        <td><?php echo $task['description']; ?></td>
-        <td><?php echo $task['date_added']; ?></td>
-        <td><?php echo $task['is_done'] ? 'Выполнено' : 'Не выполнено'; ?></td>
-        <td>
-          <a href="?id=<?php echo $task['id'] ?>&action=change">Изменить</a> 
-          <a href="?id=<?php echo $task['id'] ?>&action=done">Выполнить</a>
-          <a href="?id=<?php echo $task['id'] ?>&action=delete">Удалить</a>
-        </td>
+        <td>Описание задачи</td>
+        <td>Дата добпаления</td>
+        <td>Статус</td>
+        <td></td>
       </tr>
-    <?php endforeach; ?>
-  </tbody>
-</table>
+    </thead>
 
+    <tbody>
+      <?php foreach($taskData as $task): ?>
+        <tr>
+
+          <td><?php echo $task['description']; ?></td>
+          <td><?php echo $task['date_added']; ?></td>
+          <td><?php echo $task['is_done'] ? 'Выполнено' : 'Не выполнено'; ?></td>
+          <td>
+            <a href="?id=<?php echo $task['id'] ?>&action=change">Изменить</a> 
+            <a href="?id=<?php echo $task['id'] ?>&action=done">Выполнить</a>
+            <a href="?id=<?php echo $task['id'] ?>&action=delete">Удалить</a>
+          </td>
+
+        </tr>
+      <?php endforeach; ?>
+    </tbody>
+  </table>
+
+<?php endif; ?>
 </body>
 </html>
